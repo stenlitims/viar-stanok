29073a1e92ae1002c672ea519b302d64f107633c
Igor <stenlitims#gmail.com>
2019-08-01 13:54:56 +0300

    add distr

---
+++ dist/precache-manifest.9bf7690301bfbac0242654a8754d067e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "47ca272a9c27ab561137",
+    "url": "/css/app.d7696d03.css"
+  },
+  {
+    "revision": "47ca272a9c27ab561137",
+    "url": "/js/app.1c9ea552.js"
+  },
+  {
+    "revision": "bd2049a2c10f25fd61bd",
+    "url": "/js/chunk-2d0a49ca.3b72d5d6.js"
+  },
+  {
+    "revision": "145fe997d374ada93ce6",
+    "url": "/css/chunk-vendors.396a8939.css"
+  },
+  {
+    "revision": "145fe997d374ada93ce6",
+    "url": "/js/chunk-vendors.8704cf31.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "19e0aa987f52839cfb0c218e2c2e9694",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78ce79e725e094a1b5b61737e95ca744",
+    "url": "/data.xml"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d558e75e4cc65498b27416cec685f55e",
+    "url": "/logo.svg"
+  }
+];
